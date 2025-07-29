4f4fd32052f025111ab662e46deb560ddbbe6b00
Thad Sauter <thadsauter@gmail.com>
2025-07-29 00:02:12 -0700

    Add yabai

--- config/nvim/lua/plugins/test.lua
+++ config/nvim/lua/plugins/test.lua
@@ -1,7 +1,7 @@
 return {
   "vim-test/vim-test",
   config = function()
-    vim.g["test#ruby#rspec#executable"] = "docker compose exec spring bundle exec rspec"
+    vim.g["test#ruby#rspec#executable"] = "bin/test"
     vim.g["test#python#pytest#executable"] = "direnv exec . pytest"
     vim.api.nvim_create_user_command("TestN", "TestNearest", {})
     vim.api.nvim_create_user_command("TestF", "TestFile", {})
